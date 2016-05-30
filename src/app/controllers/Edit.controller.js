89bd17f2627bceb1396e69760f2a47f698992dce
barkhatov-den <barkhatov.den@gmail.com>
2016-05-30 18:18:43 +0300

    bugs fixing

--- src/app/controllers/Edit.controller.js
+++ src/app/controllers/Edit.controller.js
@@ -1,12 +1,37 @@
 export default function EditController($scope, $location, $stateParams){
     $scope.user = {};
-    let users = JSON.parse(localStorage.getItem('registeredUser'));
+
+    $scope.popup = {};
+
+    $scope.altInputFormats = ['M!/d!/yyyy'];
+    $scope.formats = ['MM/dd/yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        minDate: new Date(1975, 1, 1),
+        startingDay: 1
+    };
+
+    $scope.open = function() {
+        $scope.popup.opened = true;
+    };
+
+    let users;
+    try {
+        users =  JSON.parse(localStorage.getItem('registeredUser'));
+        if (!Array.isArray(users)) users = [];
+    } catch (e) {
+        users = [];
+    }
     let currentIndex;
+
+
     users.forEach(function (user, index) {
         if (user.id == $stateParams.id) {
             currentIndex = index;
             $scope.user = user;
-            $scope.user.dateOfBirth = Date(user.dateOfBirth); //ToDo!!!!!
+            $scope.user.dateOfBirth = new Date(user.dateOfBirth);
         }
     });
 
